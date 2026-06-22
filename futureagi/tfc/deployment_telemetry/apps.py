e37c42a8cd8cced8aff583055e2fbd49edbda934
Azain Khan <azain@futureagi.com>
2026-06-22 18:28:16 +0530

    log disclosure on startup

--- futureagi/tfc/deployment_telemetry/apps.py
+++ futureagi/tfc/deployment_telemetry/apps.py
@@ -5,3 +5,11 @@ class DeploymentTelemetryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tfc.deployment_telemetry"
     verbose_name = "Deployment telemetry"
+
+    def ready(self) -> None:
+        from tfc.deployment_telemetry.config import is_self_hosted_deployment
+
+        if is_self_hosted_deployment():
+            from tfc.deployment_telemetry.sender import _log_disclosure
+
+            _log_disclosure()
